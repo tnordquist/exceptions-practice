b2f5fa474140b86eaf6cf4251728eb564ca419d4
tnordquist <toddnordquist144@gmail.com>
2021-02-18 21:47:01 -0700

    Initial commit.

---
+++ src/main/java/edu/cnm/hsd/exceptions/ExceptionsDemo.java
@@ -0,0 +1,28 @@
+package edu.cnm.hsd.exceptions;
+
+import java.io.FileNotFoundException;
+
+public class ExceptionsDemo {
+
+  public static void main(String[] args) throws FileNotFoundException {
+    System.out.println("\nInside main ...");
+
+    share();
+
+    System.out.println("\nEnd of main ...");
+  }
+
+  private static void share() throws FileNotFoundException {
+    System.out.println("\nInside share ...");
+
+
+    // try {
+    HttpConnect.send(0, "hello", "http://www.goodsnips.com");
+    /*
+     * } catch (FileNotFoundException e) { // TODO Auto-generated catch block e.printStackTrace(); }
+     */
+
+    System.out.println("\nEnd of share ...");
+  }
+
+}
---
+++ src/main/java/edu/cnm/hsd/exceptions/HttpConnect.java
@@ -0,0 +1,16 @@
+package edu.cnm.hsd.exceptions;
+
+import java.io.FileNotFoundException;
+
+public class HttpConnect {
+
+  public static void send(int destination, String data, String partner)
+      throws FileNotFoundException {
+    System.out.println("\nInside send ...");
+    if (destination == 0) {
+      throw new FileNotFoundException();
+    }
+
+    System.out.println("\nEnd of send ...");
+  }
+}
