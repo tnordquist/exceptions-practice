4f60c6cf08a1b1fc5a97665b8f28f26572da3e23
tnordquist <toddnordquist144@gmail.com>
2021-02-19 22:06:22 -0700

    Finally with thrown exception.

--- src/main/java/edu/cnm/hsd/exceptions/ExceptionsDemo.java
+++ src/main/java/edu/cnm/hsd/exceptions/ExceptionsDemo.java
@@ -8,7 +8,15 @@ public class ExceptionsDemo {
   public static void main(String[] args) throws FileNotFoundException {
     System.out.println("\nInside main ...");
 
-    share();
+    try {
+      share();
+      System.out.println("After invoking share ...");
+    } catch (FileNotFoundException e) {
+      // e.printStackTrace();
+      System.out.println("main: filenotfoundexception");
+    } finally {
+      System.out.println("Inside main's finally ...");
+    }
 
     System.out.println("\nEnd of main ...");
   }
@@ -18,9 +26,10 @@ public class ExceptionsDemo {
 
 
     try {
-      HttpConnect.send(1, "hello", "http://www.goodsnips.com");
+      HttpConnect.send(0, "hello", "http://www.goodsnips.com");
       System.out.println("After invoking send ...");
     } catch (FileNotFoundException e) {
+      System.out.println("Share: filenotfoundexception ...");
       throw e;
     } catch (IOException e) {
       System.out.println("Connecting to a different server ...");
